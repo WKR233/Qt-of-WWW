873ec3753a8ea6d62d35e8f916ec25f16d6a008d
WKR233 <29503041@qq.com>
2022-06-14 20:44:09 +0800

    对主界面进行了初步设置

--- LibraryManagement/main.cpp
+++ LibraryManagement/main.cpp
@@ -1,11 +1,29 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QComboBox>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QPushButton confirm("确定",&w);
+    QComboBox identity(&w);
+    QLineEdit account(&w),password(&w);
+    QLabel l1("请选择登入身份：",&w);
+    identity.addItem("Admin");
+    identity.addItem("User");
+    account.setPlaceholderText("Please type in your account");
+    password.setPlaceholderText("Please type in your password");
+    password.setEchoMode(QLineEdit::Password);
+    l1.setGeometry(300,200,200,100);
+    identity.setGeometry(500,220,100,50);
+    account.setGeometry(400,300,400,50);
+    password.setGeometry(400,400,400,50);
+    confirm.setGeometry(600,500,100,50);
     w.show();
     return a.exec();
 }
--- LibraryManagement/mainwindow.h
+++ LibraryManagement/mainwindow.h
@@ -1,21 +1,12 @@
-#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
-
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
 };
-#endif // MAINWINDOW_H
